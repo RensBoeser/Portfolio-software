c604d58830a26d12e5370b911642a5706d17e172
Rens Boeser <rbo@bridgevest.com>
2019-06-14 15:39:35 +0200

    data update
    Co-authored-by: Noah Plazier (0929035) <0929035@hr.nl>

--- data/types.ts
+++ data/types.ts
@@ -1,3 +1,6 @@
+type TextualGrade = "O" | "V" | "G"
+type Grade = number | TextualGrade
+
 interface Member {
 	id: number
 	firstName: string
@@ -6,16 +9,24 @@ interface Member {
 	networks: {
 		github?: string
 		linkedIn?: string
+		portfolio?: string
+		[key: string]: string
 	}
+}
+
+interface ProjectMember {
+	member: Member["id"]
+	grade?: Grade
+	projectDescription?: string
 }
 
 interface Project {
 	id: number
 	title: string
 	subtitle: string
-	grade?: number | string
-	members: Array<number>
+	members: Array<ProjectMember>
 	startDate: Date
-	endDate: Date
+	endDate?: Date
 	schoolCurriculum: boolean
+	description?: string
 }
